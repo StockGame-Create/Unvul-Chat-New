11f6146aecc38bdf1a6a3390c5a3910c63bf9638
StockGame-Create <h0532148675@gmail.com>
2025-11-04 08:39:38 +0900

    Change Firebase authDomain to web.app
    Updated Firebase authDomain to use web.app

--- src/App.jsx
+++ src/App.jsx
@@ -92,7 +92,7 @@ useEffect(() => {
       
       const firebaseConfig = {
         apiKey: "AIzaSyB2I_bmwVhb-0RO8ljvunDSa3K-TCSzt2E",
-        authDomain: "unvul-chat.firebaseapp.com",
+        authDomain: "unvul-chat.web.app",
         databaseURL: "https://unvul-chat-default-rtdb.firebaseio.com",
         projectId: "unvul-chat",
         storageBucket: "unvul-chat.firebasestorage.app",
